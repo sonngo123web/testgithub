e567eac946fd58a9c52747543501ce1b257b3765
sonngo123web <sonngo@123website.vn>
2023-07-05 11:35:49 +0700

    Sửa i = 1234

--- testgithub/Program.cs
+++ testgithub/Program.cs
@@ -10,6 +10,11 @@ namespace testgithub
     {
         static void Main(string[] args)
         {
+            int i;
+            i = 123;                // Expression statement
+            Console.WriteLine(i);   // Expression statement
+            i++;                    // Expression statement
+            Console.WriteLine(i);   // Expression statement
         }
     }
 }
